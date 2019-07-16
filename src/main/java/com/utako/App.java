22cdc333a9c3b7e8c40005e6cb0befd99901b3b3
MI <15110285591@163.com>
2019-07-16 10:46:28 +0800

    update

--- src/main/java/com/utako/App.java
+++ src/main/java/com/utako/App.java
@@ -1,31 +1,43 @@
 package com.utako;
 
-import java.io.IOException;
-import org.apache.http.*;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args ) throws IOException {
-        //创建httpclient对象
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-        //指定url
-        HttpGet httpGet = new HttpGet("http://www.baidu.com/");
-        CloseableHttpResponse httpResponse = httpClient.execute(httpGet);
-        HttpEntity httpEntity = httpResponse.getEntity();
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 
-        String content = EntityUtils.toString(httpEntity, "utf-8");
-        //System.out.println(httpGet.getURI());
-        System.out.println(content);
-        httpClient.close();
+import java.io.IOException;
+
+public class App {
+    public static void main (String[] args) throws IOException {
+        try {
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+            String url = "http://zjy-hadoop-prc-ct01.bj:11201/jmx?qry=Hadoop:service=NameNode,name=NameNodeInfo";
+            HttpGet httpGet = new HttpGet(url);
+            CloseableHttpResponse httpResponse = httpClient.execute(httpGet);
+            HttpEntity httpEntity = httpResponse.getEntity();
+
+            String filepath = "/home/mi/Desktop/test1.txt";
+            InputStream is = httpEntity.getContent();
+            FileOutputStream fs = new FileOutputStream(new File(filepath));
+            byte[] bytes =  new byte[1024];
+            int length = 0;
+            while ((length = is.read(bytes)) != -1) {
+                fs.write(bytes, 0, length);
+            }
+            is.close();
+            fs.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
